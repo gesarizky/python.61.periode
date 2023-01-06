31efaf79d035d2497edd77f8ab803f9c8e59e700
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2023-01-06 13:31:22 +0700

    Add files via upload

---
+++ kode.61.periode1.py
@@ -0,0 +1,15 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+t = float(input("Masukan Nilai Waktu Getaran (s) : "))
+n = float(input("Masukan Nilai Jumlah Getaran (n) : "))
+
+# Mengkonversi
+p = t / n
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Nilai Periode (s) = ',round(p,3))
+print('==========================================================')
