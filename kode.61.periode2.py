913b4b0c32f89681de7873d239207366ddee52f1
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2023-01-13 13:29:25 +0700

    Add files via upload

---
+++ kode.61.periode2.py
@@ -0,0 +1,14 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+f = float(input("Masukan Nilai Frekuensi (Hz) : "))
+
+# Mengkonversi
+p = 1 /f
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Nilai Periode (s) = ',round(p,3))
+print('==========================================================')
