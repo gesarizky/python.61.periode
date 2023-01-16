bc76f173ec76be4b64f0e1df7611219042c92b47
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2023-01-16 12:55:28 +0700

    Add files via upload

---
+++ kode.61.periode3.py
@@ -0,0 +1,15 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+v = float(input("Masukan Nilai Kecepatan Gelombang (m/s) : "))
+l = float(input("Masukan Nilai Panjang Gelombang (m) : "))
+
+# Mengkonversi
+p = v * l
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Nilai Periode (s) = ',round(p,3))
+print('==========================================================')
